84217bc39ce9a4a52bd81245b3fef6d8bdba16ff
yusuda <yusuda@yahoo-corp.jp>
2023-03-10 19:08:00 +0900

    feat: ToDoブロックをスタイリングした

--- src/components/Notion/Block/ToDo.tsx
+++ src/components/Notion/Block/ToDo.tsx
@@ -1,25 +1,44 @@
 import { generateBlockColorClass } from "@/libs/notion/utils";
 import type { BlockComponent } from "@/types";
 import { ToDoBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import clsx from "clsx";
 import { RichText } from "./RichText";
 
 export const ToDo: BlockComponent<ToDoBlockObjectResponse> = ({
   block,
   children,
 }) => {
-  const blockType = `notion-${block.type}`;
-  const blockColor = generateBlockColorClass(block.to_do.color) ?? '';
+  const blockColor = generateBlockColorClass(block.to_do.color);
   return (
-    // TODO: BulletedListやNumberedListのようにネストさせる
-    <div className={`${blockType}_container ${blockColor}`}>
-      <input
-        type="checkbox"
-        checked={block.to_do.checked}
-        readOnly
-        className={`${blockType}`}
-      />
-      <RichText rich_text={block.to_do.rich_text} />
-      {children}
+    <div className={clsx("notion_to_do", blockColor)}>
+      <div className="notion_to_do__content">
+        <div className="notion_to_do__icon">
+          <CheckBox checked={block.to_do.checked} />
+        </div>
+        <div className="notion_to_do__title">
+          <RichText rich_text={block.to_do.rich_text} />
+        </div>
+      </div>
+      <div className="notion_to_do__children">{children}</div>
+    </div>
+  );
+};
+
+const CheckBox = ({ checked }: { checked: boolean }) => {
+  if (checked) {
+    return (
+      <div className="notion_checkbox checked">
+        <svg viewBox="0 0 14 14">
+          <polygon points="5.5 11.9993304 14 3.49933039 12.5 2 5.5 8.99933039 1.5 4.9968652 0 6.49933039"></polygon>
+        </svg>
+      </div>
+    );
+  }
+  return (
+    <div className="notion_checkbox">
+      <svg viewBox="0 0 16 16">
+        <path d="M1.5,1.5 L1.5,14.5 L14.5,14.5 L14.5,1.5 L1.5,1.5 Z M0,0 L16,0 L16,16 L0,16 L0,0 Z"></path>
+      </svg>
     </div>
   );
 };
