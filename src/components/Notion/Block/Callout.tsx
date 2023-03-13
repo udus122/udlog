9b4165c9aa7d73e0b291a00943ee66f927d8bb09
yusuda <yusuda@yahoo-corp.jp>
2023-03-13 12:45:31 +0900

    feat: calloutブロックをスタイリングした

--- src/components/Notion/Block/Callout.tsx
+++ src/components/Notion/Block/Callout.tsx
@@ -1,21 +1,27 @@
 import { generateBlockColorClass } from "@/libs/notion/utils";
 import type { BlockComponent } from "@/types";
 import { CalloutBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import clsx from "clsx";
 import { RichText } from "./RichText";
 
 export const Callout: BlockComponent<CalloutBlockObjectResponse> = ({
-  block,
+  block, children
 }) => {
-  const blockType = `notion-${block.type}`;
+  const blockType = "notion_callout";
   const blockColor = generateBlockColorClass(block.callout.color) ?? "";
   return (
-    <div id={block.id} className={`${blockType} ${blockColor}`}>
-      <span className={`${blockType}-emoji`}>
+    <div id={block.id} className={clsx("notion_block", blockType, blockColor)}>
+      <div className={clsx(`${blockType}__icon`)}>
+      <span className={clsx("notion_icon")}>
         {block.callout.icon?.type === "emoji" ? block.callout.icon.emoji : ""}
       </span>
-      <span className="notion-callout-text">
+      </div>
+      <div className={clsx(`${blockType}__content`)}>
+      <span >
         <RichText richText={block.callout.rich_text} />
       </span>
+      {children}
+      </div>
     </div>
   );
 };
