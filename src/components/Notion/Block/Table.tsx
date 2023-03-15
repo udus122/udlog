2a8156a4c358d7d65337937fb10bfa2e4d645091
yusuda <yusuda@yahoo-corp.jp>
2023-03-15 12:23:09 +0900

    refactor: 余分なコードを削除

--- src/components/Notion/Block/Table.tsx
+++ src/components/Notion/Block/Table.tsx
@@ -1,17 +1,19 @@
-import { TableBlockObjectResponse, TableRowBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import {
+  TableBlockObjectResponse,
+  TableRowBlockObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+import clsx from "clsx";
 import React from "react";
 import { RichText } from "./RichText";
 
 export const Table = ({ block }: { block: TableBlockObjectResponse }) => {
-  // @ts-ignore Notion types are incorrect
-  const children = block[block.type].children
   const has_column_header = block.table.has_column_header;
   const has_row_header = block.table.has_row_header;
   return (
-    <table id={block.id} className="notion_table">
+    <table id={block.id} className={clsx("notion_table", "notion_table")}>
       <tbody>
         {/* @ts-ignore Notion types are incorrect */}
-        {children?.map((row, i) => {
+        {block[block.type].children?.map((row, i) => {
           if (row) {
             return (
               <tr
@@ -25,7 +27,7 @@ export const Table = ({ block }: { block: TableBlockObjectResponse }) => {
                   if (cell) {
                     return (
                       <td
-                        key={row.id+j}
+                        key={row.id + j}
                         className={`notion_table_cell ${
                           has_row_header && i === 0 ? "notion_table_header" : ""
                         }`}
