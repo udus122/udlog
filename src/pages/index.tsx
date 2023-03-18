374e78e8b60194c442c3652d97b87fd1903caa23
yusuda <yusuda@yahoo-corp.jp>
2023-03-18 17:11:47 +0900

    feat: TOPページのレイアウトを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,34 +5,38 @@ import { Footer } from "@/components/Footer";
 
 import Head from "next/head";
 import { retrieveFullPage } from "@/libs/notion/page";
+import { Top } from "@/layouts/Top";
+import { collectBlockList, resolveAllChildrenBlock } from "@/libs/notion/block";
 
 export const getStaticProps = async () => {
+  const page_id = "4553dcd168664730aa8723e1cace3d7e";
   const page = await retrieveFullPage({
-    page_id: "4553dcd168664730aa8723e1cace3d7e",
+    page_id,
   });
+  const blocks =
+    (await collectBlockList({
+      block_id: page_id,
+    })) ?? [];
+  const resolvedBlocks = await resolveAllChildrenBlock(blocks);
 
   return {
     props: {
       page,
+      blocks: resolvedBlocks,
     },
-    revalidate: 60 * 60 * 24, // 1日
+    revalidate: 60 * 60 * 24,
   };
 };
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-const Index: NextPage<Props> = ({ page }) => {
-  console.log(page);
-
+const Index: NextPage<Props> = ({ page, blocks }) => {
   return (
     <>
       <Head>
         <title>UDlog</title>
       </Head>
-      <div>
-        <NavBar />
-        <Footer />
-      </div>
+      <Top page={page} blocks={blocks} />
     </>
   );
 };
