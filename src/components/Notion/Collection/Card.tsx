fad317428f86dbb63c51aed5ab9ce34bcc94f91b
yusuda <yusuda@yahoo-corp.jp>
2023-03-18 11:43:27 +0900

    feat: ギャラリービューの表示を作成

--- src/components/Notion/Collection/Card.tsx
+++ src/components/Notion/Collection/Card.tsx
@@ -3,13 +3,9 @@ import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoint
 import { noImageUrl } from "@/constants";
 import { format, parseISO } from "date-fns";
 import { Icon } from "@/components/Notion/Block/Icon";
+import { Cover } from "@/components/Notion/Page/Cover";
 
 export const Card = ({ page }: { page: PageObjectResponse }) => {
-  const coverImageUrl =
-    page.cover?.type === "file"
-      ? page.cover.file.url
-      : page.cover?.external?.url;
-
   return (
     <div id={page.id} className="notion_collection_card">
       <a
@@ -17,19 +13,19 @@ export const Card = ({ page }: { page: PageObjectResponse }) => {
         className="notion_collection_card__anchor"
       >
         <div className="notion_collection_card__cover">
-          {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
-          <img src={coverImageUrl ? coverImageUrl : noImageUrl} />
+          <Cover cover={page.cover} alternativeImageUrl={noImageUrl} />
         </div>
         <div className="notion_collection_card__content">
-          <div className="notion_property notion_property__title">
+          <div className="notion_property_item notion_property_item__title">
             <Icon icon={page.icon} />
             <span>{extractTitleFromPageOrDatabase(page)}</span>
           </div>
-          <div className="notion_property notion_property_list">
+          <div className="notion_property_list">
             {/* NOTE: 最終的にはすべてのプロパティを走査して、propsでフィルターを掛けられるようにしたい。ひとまず、最終更新日を表示する */}
-            <div>最終更新日</div>
-            <div className="notion_property notion_property__date">
-              {format(parseISO(page.last_edited_time), "yyyy-MM-dd")}
+            <div className="notion_property_item">
+              <div className="notion_property_item__date">
+                {format(parseISO(page.last_edited_time), "yyyy-MM-dd")}
+              </div>
             </div>
           </div>
         </div>
