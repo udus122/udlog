7fe5f44bb1e56fd5d5e60bc6be4f5a3418c8373c
yusuda <yusuda@yahoo-corp.jp>
2023-03-27 12:47:54 +0900

    chore: jestにpath aliasを設定

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,9 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1", // 追加
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
